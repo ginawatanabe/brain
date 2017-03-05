fee5f298c574d2eb2de016abaeee980717e2d260
Gina W <watanabegina26@gmail.com>
2017-03-04 16:57:09 -1000

    Added orbit and drag controls using 3js libraries.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
+
 window.onload = function() {
-  let scene, camera, renderer;
+  let scene, camera, renderer, controls;
 
   let WIDTH  = window.innerWidth;
   let HEIGHT = window.innerHeight;
@@ -7,7 +8,7 @@ window.onload = function() {
   let SPEED = 0.01;
 
   let raycaster = new THREE.Raycaster();
-  let mouse = new THREE.Vector2();
+  let mouse = new THREE.Vector2(), INTERSECTED;
 
   function init() {
     scene = new THREE.Scene();
@@ -16,12 +17,25 @@ window.onload = function() {
     initRenderer();
     initMesh();
     initLight();
-
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.rotateSpeed= 0.3;
+    controls.zoomSpeed = 1.2;
+    controls.panSpeed = 0.8;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
+    // controls = new THREE.TrackballControls(camera);
+    controls.addEventListener('change', render);
     document.body.appendChild(renderer.domElement);
+
+    //Initiate drag controls.
+    let dragControls = new THREE.DragControls( scene.children, camera, renderer.domElement );
+	  dragControls.addEventListener( 'dragstart', function ( event ) { controls.enabled = false; } );
+		dragControls.addEventListener( 'dragend', function ( event ) { controls.enabled = true; } );
+
   }
 
   function initCamera() {
-    camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 10);
+    camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 1000);
     camera.position.set(0, 3.5, 5);
     camera.lookAt(scene.position);
   }
@@ -40,7 +54,7 @@ window.onload = function() {
 
   function initMesh() {
     let loader = new THREE.JSONLoader();
-    for (let i = 0; i<5; i++) {
+    for (let i = 0; i<2; i++) {
       loader.load('models/purpleblocksculpt.json', function(geometry, materials) {
         mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial(materials));
         // mesh.scale.x = mesh.scale.y = mesh.scale.z = 0.75;
@@ -73,22 +87,32 @@ window.onload = function() {
   function onMouseMove(event) {
     mouse.x = (event.clientX/window.innerWidth)*2 - 1;
     mouse.y = -(event.clientY/window.innerHeight)*2 + 1;
+
     // Raycaster
     // Update the picking ray with the camera and mouse position.
     raycaster.setFromCamera(mouse, camera);
     // Calculate objects intersecting the picking ray
     let intersects = raycaster.intersectObjects(scene.children);
+    // let currentColor;
+    // currentColor = scene.children[0].color.getRGB();
 
     if (intersects.length) {
         console.log("hello");
+        // console.log(mouse);
+
+        intersects[0].object.material.materials[0].emissive.setRGB(0.2,0,0.5);
     }
+
+    // if (intersects.length == 0)
+      // console.log("YO");
+      // console.log(scene.children[0]);
+      // scene.children[0].material.color = 1.2;
   }
 
   function render() {
-      requestAnimationFrame(render);
-      // rotateCube();
-      rotateMesh();
-      renderer.render(scene, camera);
+    requestAnimationFrame(render);
+    // rotateMesh();
+    renderer.render(scene, camera);
   }
   window.addEventListener('mousemove',onMouseMove,false);
 
